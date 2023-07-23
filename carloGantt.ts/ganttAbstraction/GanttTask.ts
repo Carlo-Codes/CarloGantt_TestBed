dad0ae5e2b18800d7446e8befd56328848f4ab27
Carlo <carlo.scrocca@googlemail.com>
2023-07-23 13:41:20 +0100

    adding task details text

--- carloGantt.ts/ganttAbstraction/GanttTask.ts
+++ carloGantt.ts/ganttAbstraction/GanttTask.ts
@@ -1,4 +1,4 @@
-import { Graphics } from "pixijs"
+import { Graphics, Text, TextStyle } from "pixijs"
 import { renderSettings, taskType } from "../types/generalTypes"
 import dayjs, { Dayjs } from "dayjs"
 
@@ -9,32 +9,41 @@ export default class GanttTask{
     private detailsPositionX : number
     private detailsPostionY : number
     private detailsWidth : number
+    private detailTextStyle:TextStyle
 
     private rowBodyPositionX : number
     private rowBodypositionY : number
     private rowWidth : number
     private rowHeight : number
 
-    private detailsName : string
     private detailsStartTime : Dayjs
     private detailsEndTime : Dayjs
 
     private detailsRect:Graphics
     private rowRect:Graphics
+    private detailsNameText: Text
 
     private lineWeight:number
     private lineColour:number
 
     constructor(task:taskType, positionX:number, positionY:number, height:number, rowWidth:number, rowHeight:number, detailsWidth:number){
         
+        this.detailTextStyle = new TextStyle({
+            align: "center",
+            fill: "#754c24",
+            fontSize: 10
+        });
+        this.lineWeight = 1
+        this.lineColour = 0x00FF00
+
         this.task = task
         this.rowHeight = rowHeight
         
+
         this.detailsPositionX = positionX
         this.detailsPostionY = positionY
         this.detailsWidth = detailsWidth
 
-        this.detailsName = task.name
         this.detailsStartTime = dayjs(task.startDate)
         this.detailsEndTime = dayjs(task.endDate)
 
@@ -44,10 +53,13 @@ export default class GanttTask{
 
         this.detailsRect = new Graphics()
         this.rowRect = new Graphics()
+        this.detailsNameText = new Text(this.task.name, this.detailTextStyle)
+        this.detailsNameText.position.set(this.detailsPositionX, this.detailsPostionY)
+
+
 
         //tests/// - to be turned into some sort of object for passing around
-        this.lineWeight = 1
-        this.lineColour = 0x00FF00
+
     }
 
     render(){
@@ -55,6 +67,8 @@ export default class GanttTask{
         this.detailsRect.drawRect(this.detailsPositionX, this.detailsPostionY, this.detailsWidth, this.rowHeight)
         this.rowRect.lineStyle(this.lineWeight, this.lineColour)
         this.rowRect.drawRect(this.rowBodyPositionX, this.rowBodypositionY, this.rowWidth, this.rowHeight)
+
+        this.detailsRect.addChild(this.detailsNameText)
     }
 
     getDetailsRect(){
