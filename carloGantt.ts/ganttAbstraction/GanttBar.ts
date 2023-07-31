696126572356c79c4f42d1ee6860fcf0b306f6c4
CeeLizzle <88803350+CeeLizzle@users.noreply.github.com>
2023-07-31 22:38:26 +0100

    adding arrows

--- carloGantt.ts/ganttAbstraction/GanttBar.ts
+++ carloGantt.ts/ganttAbstraction/GanttBar.ts
@@ -1,8 +1,12 @@
-import { Graphics, FederatedPointerEvent,Rectangle} from "pixijs";
+import { Graphics, FederatedPointerEvent,Rectangle,Sprite,Container} from "pixijs";
 
 
 export default class GanttBar {
     private bar: Graphics
+    private arrows:Container
+    private leftArrow:Sprite
+    private rightArrow:Sprite
+
     private positionX: number | null
     private positionY: number | null
     private height: number | null
@@ -11,14 +15,26 @@ export default class GanttBar {
 
     constructor(){
         this.bar = new Graphics()
+        this.arrows = new Container()
+        this.rightArrow = Sprite.from("carloGantt.ts/Assets/Arrow.png")
+        this.rightArrow.anchor.set(0.5,0.5)
+        this.leftArrow = Sprite.from("carloGantt.ts/Assets/Arrow.png")
+        this.leftArrow.anchor.set(0.5,0.5)
+        this.leftArrow.rotation = 3
+        this.rightArrow.scale.set(0.1,0.1)
+        this.leftArrow.scale.set(0.1,0.1)
+
         this.positionX = null
         this.positionY = null
         this.height = null
         this.width = null
+
         this.bar.interactive = true
         this.bar.onmouseover = this.onHover.bind(this)
         this.bar.onmouseleave = this.onOffHover.bind(this)
-        
+
+        this.arrows.addChild(this.rightArrow,this.leftArrow)
+        this.bar.addChild(this.arrows)
     }
 
     setBar(x:number, y:number, height:number, width:number){
@@ -26,12 +42,18 @@ export default class GanttBar {
         this.positionX = x
         this.positionY = y + (height/4)
         this.width = width
+
         this.bar.beginFill(0x00FF00)
         this.bar.drawRect(this.positionX,this.positionY,this.width,this.height)
         this.bar.hitArea = new Rectangle(this.positionX,this.positionY,this.width,this.height)
         
-        
-        
+        this.leftArrow.x = this.positionX
+        this.leftArrow.y = y + (height/2)
+        this.rightArrow.x = this.positionX + this.width
+        this.rightArrow.y = y + (height/2)
+    }
+    generateArrows(){
+
     }
 
     getBar(){
