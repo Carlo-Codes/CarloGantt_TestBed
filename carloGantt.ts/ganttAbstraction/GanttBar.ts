6af1f47c4a43186effc7bdb31f4b9c582cd8234d
CeeLizzle <88803350+CeeLizzle@users.noreply.github.com>
2023-08-01 19:03:33 +0100

    hit area of arrows

--- carloGantt.ts/ganttAbstraction/GanttBar.ts
+++ carloGantt.ts/ganttAbstraction/GanttBar.ts
@@ -21,8 +21,8 @@ export default class GanttBar {
         this.leftArrow = Sprite.from("carloGantt.ts/Assets/Arrow.png")
         this.leftArrow.anchor.set(0.5,0.5)
         this.leftArrow.rotation = 3
-        this.rightArrow.scale.set(0.1,0.1)
-        this.leftArrow.scale.set(0.1,0.1)
+        this.rightArrow.scale.set(0.05,0.05)
+        this.leftArrow.scale.set(0.05,0.05)
 
         this.positionX = null
         this.positionY = null
@@ -30,11 +30,21 @@ export default class GanttBar {
         this.width = null
 
         this.bar.interactive = true
-        this.bar.onmouseover = this.onHover.bind(this)
-        this.bar.onmouseleave = this.onOffHover.bind(this)
+        
+        this.leftArrow.interactive = true
+        this.rightArrow.interactive = true
+        this.bar.onmouseover = this.onBarOver.bind(this)
+        this.bar.onmouseleave = this.onBarLeave.bind(this)
+
+        this.arrows.onmouseover = this.onArrowOver.bind(this)
+        this.arrows.onmouseleave = this.onArrowleave.bind(this)
 
         this.arrows.addChild(this.rightArrow,this.leftArrow)
+        
+
         this.bar.addChild(this.arrows)
+
+        this.arrows.alpha = 0
     }
 
     setBar(x:number, y:number, height:number, width:number){
@@ -45,14 +55,19 @@ export default class GanttBar {
 
         this.bar.beginFill(0x00FF00)
         this.bar.drawRect(this.positionX,this.positionY,this.width,this.height)
-        this.bar.hitArea = new Rectangle(this.positionX,this.positionY,this.width,this.height)
+        this.bar.hitArea = new Rectangle(this.positionX,this.positionY,this.width,this.height).pad(15)
+        
         
         this.leftArrow.x = this.positionX
         this.leftArrow.y = y + (height/2)
         this.rightArrow.x = this.positionX + this.width
         this.rightArrow.y = y + (height/2)
-    }
-    generateArrows(){
+
+        this.leftArrow.hitArea = this.leftArrow.getBounds().pad(15)
+        this.rightArrow.hitArea = this.rightArrow.getBounds().pad(15)
+        console.log(this.leftArrow.hitArea)
+       
+        //possible probems with order of hit areas and children etc
 
     }
 
@@ -60,12 +75,29 @@ export default class GanttBar {
         return this.bar
     }
 
-    onHover(e:FederatedPointerEvent){
-        this.bar.alpha = 0.5
+    onBarOver(e:FederatedPointerEvent){
+        this.arrows.alpha = 1
     }
 
-    onOffHover(e:FederatedPointerEvent){
-        this.bar.alpha = 1
+    onBarLeave(e:FederatedPointerEvent){
+        this.arrows.alpha = 0
+    }
+
+    onArrowOver(e:FederatedPointerEvent){
+        console.log("fire")
+        const body = document.getElementById('body')
+        if(body){
+            body.style.cursor = 'col-resize'
+        }
+        console.log("fire")
+    }
+    onArrowleave(e:FederatedPointerEvent){
+        console.log("fire")
+        const body = document.getElementById('body')
+        if(body){
+            body.style.cursor = 'pointer'
+        }
+        console.log("fire")
     }
 
 
